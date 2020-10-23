0de2f55b38a0acc4465b56628fb4c1a78e3a5296
장문성/선임연구원/Protocol실 1팀(ms.jang@lge.com) <ms.jang@lge.com>
2020-10-23 19:00:28 +0900

    지진

--- 101_지진대피.cpp
+++ 101_지진대피.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define MAX_BOX 500
+#define MAX_MAP 501
 
 using namespace std;
 
@@ -9,8 +10,9 @@ int Sy; // 지게차의 최초 행위치
 int BoxX[12000]; // 자재박스의 열위치
 int BoxY[12000]; // 자재박스의 행위치
 
-int map[510][510];
-int visited[510][510];
+
+int map[MAX_MAP][MAX_MAP];
+int visited[MAX_MAP][MAX_MAP];
 
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
@@ -18,19 +20,19 @@ int dy[] = { -1,1,0,0 };
 //queue
 
 struct Point {
-	int x;
-	int y;
-	int t;
-	Point(int x, int y, int t) :x(x), y(y),t(t) {};
+	int x = 0;
+	int y = 0;
+	int t = 0;
 };
 
+Point start;
 Point que[500 * 500 * 4];
-int rp, wp;
+int rp;
+int wp;
 
 void push(Point p) {
 	que[wp] = p;
 	wp++;
-	visited[p.y][p.x] = p.t;
 }
 
 void pop() {
@@ -41,7 +43,7 @@ Point top() {
 	return que[rp];
 }
 
-int IsEmpty(){
+int IsEmpty() {
 	return wp == rp;
 }
 
@@ -49,33 +51,32 @@ void SetInit() {
 	for (int i = 0; i < N; i++) {
 		map[BoxY[i]][BoxX[i]] = 1;
 	}
-	for (int y = 1; y < 501; y++) {
-		for (int x = 1; x < 501; x++) {
-			visited[y][x] = MAX_BOX;
+}
+
+void BFS(Point start) {
+	visited[start.y][start.x] = 1;
+	start.t = 0;
+	push(start);
+	while (wp > rp) {
+		Point tmp = que[rp++];
+		if (visited[tmp.y][tmp.x] < tmp.t) continue;
+		for (int i = 0; i < 4; i++) {
+			int nx = tmp.x + dx[i];
+			int ny = tmp.y + dy[i];
+			if (nx<0 || nx>MAX_MAP-1 || ny<0 || ny>MAX_MAP-1)continue;
+			if (visited[ny][nx] == 0 || visited[tmp.y][tmp.x] + map[ny][nx] < visited[ny][nx]) {
+				visited[ny][nx] = visited[tmp.y][tmp.x] + map[ny][nx];
+				tmp.x = nx;
+				tmp.y = ny;
+				tmp.t = visited[ny][nx];
+				push(tmp);
+			}
 		}
 	}
 }
 
-void BFS(int x, int y, int t) {
-	Point tmp(x, y, t);
-	for (int i = 0; i < 4; i++) {
-		tmp.x = +dx[i];
-		tmp.y =+ dy[i];
-		tmp.t = +map[tmp.y][tmp.x];
-		if (visited[tmp.y][tmp.x] <= t)
-			continue;
-		push(tmp);
-	}
-	while (!IsEmpty()) {
-		tmp = top();
-		pop();
-
-		BFS(tmp.x, tmp.y, tmp.t);
-	}
-}
-
 void InputData(void) {
-	cin >> N >> Sx >> Sy;
+	cin >> N >> start.x >> start.y;
 	for (int i = 0; i < N; i++) {
 		cin >> BoxX[i] >> BoxY[i];
 	}
@@ -85,9 +86,9 @@ int main(void) {
 	int ans = -1;
 	InputData(); // 입력함수	
 	SetInit();
+	BFS(start);
+	ans = visited[1][1];
 	//	코드를 작성하세요
-
-	cout << ans << endl;//출력
+	cout << ans - 1 << endl;//출력
 	return 0;
 }
-
